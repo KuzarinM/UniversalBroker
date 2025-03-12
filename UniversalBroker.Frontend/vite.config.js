f4bbadabec09a17d5866ecdfa6091f82a83b7d7b
KuzarinM <kuzarinm@mail.ru>
2025-03-12 22:33:55 +0300

    Маленькая доработка, которая избавит нас от cors навсегда. Спасибо Proxy!

--- UniversalBroker.Frontend/vite.config.js
+++ UniversalBroker.Frontend/vite.config.js
@@ -4,4 +4,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '^/proxy/.*': {
+        target: 'https://localhost:7071',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/proxy/, '')
+      }
+    }
+  }
 })
